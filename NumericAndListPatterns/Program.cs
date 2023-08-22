e4f4f140781c56ede57e971d862b3cfd10a7ab39
Santoshkumargupta <santoke100@gmai.com>
2023-08-22 16:21:46 +0545

    some file of code is added

---
+++ NumericAndListPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Numerics;
+
+var result = AddAll(new[] { 1, 2, 3, 4, 5 });
+
+Console.WriteLine(result);
+
+T AddAll<T>(T[] values) where T : INumber<T> => values switch
+{
+    [] => T.Zero,
+    [var first,.. var rest] => first + AddAll(rest)
+
+};
